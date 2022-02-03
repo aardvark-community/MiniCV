5f1877307e36dfed76ef68e5e6783be8dfde3ac4
krauthaufen <krauthaufen@awx.at>
2022-02-03 23:47:16 +0100

    windows fix

--- buildnative.cmd
+++ buildnative.cmd
@@ -8,7 +8,7 @@ cmd /C ".vcpkg\vcpkg\bootstrap-vcpkg.bat -disableMetrics"
 
 
 SET VCPKG_NUGET_REPOSITORY="https://github.com/aardvark-community/MiniCV"
-.vcpkg\vcpkg\vcpkg.exe install OpenCV --triplet x64-windows-static-md --binarysource='clear;nuget,Github,readwrite'
+.vcpkg\vcpkg\vcpkg.exe install OpenCV --triplet x64-windows-static-md --binarysource="clear;nuget,Github,readwrite"
 
 cmake -S src\MiniCVNative -B src\MiniCVNative\build -DCMAKE_TOOLCHAIN_FILE="%~dp0\.vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static-md -DCMAKE_BUILD_TYPE=Release
 cmake --build src\MiniCVNative\build --config Release --target install
