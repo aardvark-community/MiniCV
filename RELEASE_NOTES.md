5a0e260c8bd79b96daf42c09eb46c136991fc76c
aszabo314 <aszabo@vrvis.at>
2022-04-15 14:02:34 +0200

    contrib

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,6 +1,6 @@
 ### 1.1.9
 * Added SURF, SIFT etc.
-* added SolvePnPRansac & Refine
+* added SolvePnPRansac & Refine 
 
 ### 1.1.8
 * updated Aardvark.Base to 5.2
--- buildnativelocal.cmd
+++ buildnativelocal.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-.config/nuget.exe setApiKey -Source GitHub $GITHUB_TOKEN -NonInteractive
-cmd /C "rmdir /S /Q .vcpkg"
+REM .config/nuget.exe setApiKey -Source GitHub $GITHUB_TOKEN -NonInteractive
+REM cmd /C "rmdir /S /Q .vcpkg"
 
 git clone https://github.com/Microsoft/vcpkg.git ./.vcpkg/vcpkg --depth 1
 copy .vcpkg\vcpkg\triplets\community\x64-windows-static-md.cmake .vcpkg\vcpkg\triplets\community\x64-windows-static-md-rel.cmake
@@ -9,8 +9,9 @@ echo set(VCPKG_BUILD_TYPE release) >> .vcpkg\vcpkg\triplets\community\x64-window
 cmd /C ".vcpkg\vcpkg\bootstrap-vcpkg.bat -disableMetrics"
 
 
-SET VCPKG_NUGET_REPOSITORY=https://github.com/aardvark-community/MiniCV
-.vcpkg\vcpkg\vcpkg.exe install OpenCV[contrib] --triplet x64-windows-static-md-rel --binarysource="clear;nuget,Github,readwrite;nugettimeout,1000"
+REM SET VCPKG_NUGET_REPOSITORY=https://github.com/aardvark-community/MiniCV
+.vcpkg\vcpkg\vcpkg.exe install OpenCV[contrib] --triplet x64-windows-static-md-rel 
+REM --binarysource="clear;nuget,Github,readwrite;nugettimeout,1000"
 
 cmake -S src\MiniCVNative -B src\MiniCVNative\build -DCMAKE_TOOLCHAIN_FILE="%~dp0\.vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static-md-rel -DCMAKE_BUILD_TYPE=Release
 cmake --build src\MiniCVNative\build --config Release --target install
