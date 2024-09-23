455f6b7bd46c0c96172d6874bfee5ed092e147d0
aszabo314 <aszabo@vrvis.at>
2024-09-23 13:26:35 +0200

    ### 1.1.16 * updated dependencies

--- src/Test/Program.fs
+++ src/Test/Program.fs
@@ -64,7 +64,7 @@ let main argv =
     let data = c.GetByteArrayAsync("https://images.pexels.com/photos/245535/pexels-photo-245535.jpeg?cs=srgb&dl=pexels-snapwire-245535.jpg&fm=jpg").Result
     let img = 
         use ms = new MemoryStream(data)
-        PixImageSharp.Create(ms).ToPixImage<byte>(Col.Format.RGBA)
+        Aardvark.Data.PixImageSharp.Create(ms).ToPixImage<byte>(Col.Format.RGBA)
 
     let modes =
         [
